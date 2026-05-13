3fd2b4a2220d37e647eaaf2e693c8ecb25e6f563
AKSHAY-MORE10 <akshaybapumore10@gmail.com>
2026-05-13 14:38:04 +0530

    full updated

---
+++ Flask/main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request, url_for, jsonify
+
+app = Flask(__name__, static_folder='assets')
+
+
+@app.route('/')
+def home():
+        
+    return render_template('index.html')
+     
+
+
+
+if __name__ == '__main__':
+     app.run(debug=True)
+    
